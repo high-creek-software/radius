b2d60c63698d6e268350c7543257dbc403b8eeab
Kendell Fabricius <kendell@highcreeksoftware.io>
2024-06-07 06:05:52 -0600

    Added the ability to import the tigerlines .dbf file.

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/high-creek-software/radius
 go 1.21.3
 
 require (
+	github.com/Valentin-Kaiser/go-dbase v1.12.0
 	github.com/jackc/pgx/v5 v5.5.0
 	github.com/shaxbee/go-spatialite v0.0.0-20180425212100-9b4c81899e0e
 )
@@ -14,5 +15,6 @@ require (
 	github.com/mattn/go-sqlite3 v1.14.22 // indirect
 	golang.org/x/crypto v0.9.0 // indirect
 	golang.org/x/sync v0.1.0 // indirect
-	golang.org/x/text v0.9.0 // indirect
+	golang.org/x/sys v0.19.0 // indirect
+	golang.org/x/text v0.14.0 // indirect
 )
--- manager.go
+++ manager.go
@@ -3,12 +3,22 @@ package radius
 import (
 	"encoding/csv"
 	"errors"
+	"fmt"
+	"github.com/Valentin-Kaiser/go-dbase/dbase"
 	"io"
 	"log/slog"
 )
 
+// Headers as per the file obtained here: https://www.census.gov/cgi-bin/geo/shapefiles/index.php
+const (
+	headerZCTA = "ZCTA5CE20"
+	headerLat  = "INTPTLAT20"
+	headerLon  = "INTPTLON20"
+)
+
 type Manager interface {
 	ImportZCTAs(r io.Reader) error
+	ImportZCTAsFromDBF(fileName string) error
 }
 
 type ManagerImpl struct {
@@ -47,6 +57,60 @@ func (m *ManagerImpl) ImportZCTAs(r io.Reader) error {
 		if err != nil {
 			slog.Error("error inserting point", "err", err)
 		}
+	}
+
+	return nil
+}
+
+func (m *ManagerImpl) ImportZCTAsFromDBF(fileName string) error {
+
+	/*table, err := godbf.NewFromFile(fileName, "UTF8")
+	if err != nil {
+		return fmt.Errorf("error opening file: %w", err)
+	}
+
+	for i := 0; i < table.NumberOfRecords(); i++ {
+		zip, zipErr := table.FieldValueByName(i, headerZCTA)
+		lat, latErr := table.FieldValueByName(i, headerLat)
+		lon, lonErr := table.FieldValueByName(i, headerLon)
+
+		if zipErr == nil && latErr == nil && lonErr == nil {
+			// Formatting the point as WKT (well known text), this matches what is available via the above csv
+			point := fmt.Sprintf("POINT(%s %s)", lon, lat)
+			slog.Info("Record", "zip", zip, "POINT", point)
+			if err := m.repository.Store(zip, point); err != nil {
+				slog.Error("error importing zip", "error", err)
+			}
+		} else {
+			slog.Error("Error reading record", "row", i, "zipErr", zipErr, "latErr", latErr, "lonErr", lonErr)
+		}
+	}*/
+
+	table, err := dbase.OpenTable(&dbase.Config{
+		Filename: fileName,
+		Untested: true,
+	})
+	if err != nil {
+		return fmt.Errorf("error opening file: %w", err)
+	}
+
+	for !table.EOF() {
+		row, err := table.Next()
+		if err != nil {
+			return err
+		}
+
+		zip := row.FieldByName(headerZCTA).GetValue().(string)
+		lat := row.FieldByName(headerLat).GetValue().(string)
+		lon := row.FieldByName(headerLon).GetValue().(string)
+
+		// Formatting the point as WKT (well known text), this matches what is available via the above csv
+		point := fmt.Sprintf("POINT(%s %s)", lon, lat)
+		slog.Info("Record", "zip", zip, "POINT", point)
+		if err := m.repository.Store(zip, point); err != nil {
+			slog.Error("error importing zip", "error", err)
+		}
+
 	}
 
 	return nil
